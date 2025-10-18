cc4eb1fde13088884c8e4f8b5483c4eb6d408b91
Morten Gerdes <mortengerdes7000@gmail.com>
2025-10-18 21:27:50 +0200

    Lock Neotest-go to version

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -1,73 +1,80 @@
 return {
-    {
-        "ray-x/go.nvim",
-        ft = { "go", "gomod" },
-        dependencies = { -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        build = ':lua require("go.install").update_all()', -- if you need to install/update all binaries
-        keys = {
-            { "<leader>cs", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
-            { "<leader>ce", "<cmd>GoIfErr<cr>",      desc = "Go if err" },
-        },
-        opts = {
-            run_in_floaterm = true,
-            floaterm = {
-                posititon = "bottom",
-                height = 0.4,
-            },
-        },
-        config = function(_, opts)
-            require("go").setup(opts)
+	{
+		"ray-x/go.nvim",
+		ft = { "go", "gomod" },
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		build = ':lua require("go.install").update_all()', -- if you need to install/update all binaries
+		keys = {
+			{ "<leader>cs", "<cmd>GoFillStruct<cr>", desc = "Go fill struct" },
+			{ "<leader>ce", "<cmd>GoIfErr<cr>", desc = "Go if err" },
+		},
+		opts = {
+			run_in_floaterm = true,
+			floaterm = {
+				posititon = "bottom",
+				height = 0.4,
+			},
+		},
+		config = function(_, opts)
+			require("go").setup(opts)
 
-            local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*.go",
-                callback = function()
-                    require("go.format").goimport()
-                end,
-                group = format_sync_grp,
-            })
-        end,
-    },
-    {
-        "leoluz/nvim-dap-go",
-        ft = { "go", "gomod" },
-        config = function()
-            require("dap-go").setup({
-                delve = {
-                    build_flags = "-tags=integration",
-                },
-            })
+			local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*.go",
+				callback = function()
+					require("go.format").goimport()
+				end,
+				group = format_sync_grp,
+			})
+		end,
+	},
+	{
+		"leoluz/nvim-dap-go",
+		ft = { "go", "gomod" },
+		config = function()
+			require("dap-go").setup({
+				delve = {
+					build_flags = "-tags=integration",
+				},
+			})
             -- stylua: ignore
             vim.keymap.set("n", "<leader>td", "<cmd>lua require('dap-go').debug_test()<cr>",
                 { desc = "Debug test", silent = true })
-        end,
-    },
-    {
-        "nvim-neotest/neotest",
-        optional = true,
-        dependencies = {
-            "fredrikaverpil/neotest-golang",
-        },
-        opts = {
-            adapters = {
-                ["neotest-golang"] = {
-                    go_test_args = { "-v", "-race", "-count=1", "-tags=integration" },
-                },
-            },
-        },
-        config = function()
-            local conf = {
-                runner = "gotestsum",
-            }
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-golang")(conf),
-                },
-            })
-        end,
-    },
+		end,
+	},
+	{
+		"nvim-neotest/neotest",
+		optional = true,
+		dependencies = {
+			{
+				"fredrikaverpil/neotest-golang",
+                -- Long story short: Trees(h)itter has deprecated the master branch and is migrating to main branch
+                -- In that regard, they decided it was a good idea to make huge breaking changes.
+                -- fredrikaverpil's version 2 of neotest-golang tries to support this but fails miserably
+                -- So Locking to this version for now as it seems stable
+                version = "v1.15.1",
+			},
+		},
+		opts = {
+			adapters = {
+				["neotest-golang"] = {
+					go_test_args = { "-v", "-race", "-count=1", "-tags=integration" },
+				},
+			},
+		},
+		config = function()
+			local conf = {
+				runner = "gotestsum",
+			}
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang")(conf),
+				},
+			})
+		end,
+	},
 }
