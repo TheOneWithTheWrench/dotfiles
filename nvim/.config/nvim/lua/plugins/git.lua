5dda50c0db39ed3682edfea2686f7218b446bc88
Morten Gerdes <mgd@lunar.app>
2025-10-21 11:27:46 +0200

    Setup git blame

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -2,11 +2,11 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		keys = {
-			{ "<leader>hd", "<cmd>Gitsigns diffthis<cr>", { desc = "Show difference" } },
+			{ "<leader>hd", "<cmd>Gitsigns diffthis<cr>",  desc = "Show difference"  },
+			{ "<leader>hb", "<cmd>Gitsigns blame<cr>",  desc = "Git Blame"  },
 		},
 		opts = {
-			current_line_blame = true,
-  			signs_visible = false,
+			current_line_blame = false, -- This is the grey text after a line... Kinda useless
 		},
 	},
 }
