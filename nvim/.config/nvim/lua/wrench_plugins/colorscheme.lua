2e7b1c2281768d8aefa67099c4d8843b59a8dc45
Morten Gerdes <mgd@lunar.app>
2026-03-31 13:27:29 +0200

    feat: transparent status line

--- nvim/.config/nvim/lua/wrench_plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/wrench_plugins/colorscheme.lua
@@ -9,5 +9,19 @@ return {
             transparent_mode = true,
         })
         vim.cmd.colorscheme("gruvbox")
+
+        -- Make statusline transparent
+        -- Active status line
+        vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+        -- Inactive status line
+        vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+        -- prevent overrides by theme
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = function()
+                vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE" })
+                vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE" })
+            end,
+        })
     end,
 }
