c99790244648c4afd51e051ad70f04cd8324c051
Morten Gerdes <mgd@lunar.app>
2026-06-14 13:22:35 +0200

    feat: trying out 99

---
+++ nvim/.config/nvim/lua/wrench_plugins/99.lua
@@ -0,0 +1,51 @@
+return {
+    {
+        url = "https://github.com/ThePrimeagen/99",
+        dependencies = {
+            { url = "https://github.com/saghen/blink.compat" },
+        },
+        keys = {
+            {
+                mode = { "v" },
+                lhs = "<leader>9v",
+                rhs = function() require("99").visual() end,
+                desc = "99 Visual",
+            },
+            {
+                mode = { "n" },
+                lhs = "<leader>9o",
+                rhs = function() require("99").open() end,
+                desc = "99 Open previous results",
+            },
+            {
+                mode = { "n" },
+                lhs = "<leader>9x",
+                rhs = function() require("99").stop_all_requests() end,
+                desc = "99 Stop All Requests",
+            },
+            {
+                mode = { "n" },
+                lhs = "<leader>9s",
+                rhs = function() require("99").search() end,
+                desc = "99 Search",
+            },
+        },
+        config = function()
+            local _99 = require("99")
+            _99.setup({
+                provider = _99.Providers.CursorAgentProvider,
+                model = "composer-2.5-fast",
+                completion = {
+                    custom_rules = {
+                        "scratch/custom_rules/",
+                    },
+                    source = "blink",
+                },
+                md_files = {
+                    "AGENT.md",
+                    "CLAUDE.md",
+                },
+            })
+        end,
+    },
+}
